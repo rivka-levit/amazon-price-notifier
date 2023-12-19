bfef3e4dc304230d5f45be5f0852243b154a454a
rivka-yuliia <juliacreadora@gmail.com>
2023-12-20 01:42:59 +0200

    templates pages

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        return redirect('started.html')
+
+
+@app.route('/started.html')
+def started():
+    return render_template('started.html')
 
 
 if __name__ == '__main__':
