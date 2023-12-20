cd2e113f4febc756c77e41296591b59d34aa05db
rivka-yuliia <juliacreadora@gmail.com>
2023-12-20 16:43:33 +0200

    clean data and tests

--- app/tests/test_crawler.py
+++ app/tests/test_crawler.py
@@ -22,3 +22,46 @@ class TestCrawler(TestCase):
                             'disable-blink-features=AutomationControlled']
 
         self.assertEqual(self.crawler.options.arguments, expected_options)
+
+    def test_clean_data_return_float(self):
+        """Test clean_data method returns a float number."""
+
+        data = ('16.', '35')
+        expected_result = float(f'{data[0]}{data[1]}')
+
+        result = self.crawler.clean_data(*data)
+
+        self.assertIsInstance(result, float)
+        self.assertEqual(result, expected_result)
+
+    def test_clean_data_unexpected_arg_error(self):
+        """Test clean_data raise error with not enough arguments."""
+
+        data = '16'
+
+        with self.assertRaises(ValueError):
+            self.crawler.clean_data(data)
+
+    def test_clean_data_unexpected_type_arg_error(self):
+        """Test clean_data raise error with not string type argument."""
+
+        data = ('16.', 35)
+
+        with self.assertRaises(ValueError):
+            self.crawler.clean_data(data)
+
+    def test_clean_data_unexpected_first_value_error(self):
+        """Test clean_data raise error with wrong first argument."""
+
+        data = ('1', '6')
+
+        with self.assertRaises(ValueError):
+            self.crawler.clean_data(data)
+
+    def test_clean_data_convert_float_error(self):
+        """Test clean_data raise error when converting to float a wrong value."""
+
+        data = ('rrf', 'asdf')
+
+        with self.assertRaises(ValueError):
+            self.crawler.clean_data(data)
--- app/utils/crawler.py
+++ app/utils/crawler.py
@@ -13,17 +13,15 @@ class AbstractCrawler(ABC):
 class AmazonPriceCrawler (AbstractCrawler):
     """Scrape price from Amazon product page."""
 
-    _options = webdriver.ChromeOptions()
-
     def __init__(self):
-        AmazonPriceCrawler.set_options()
+        self._options = webdriver.ChromeOptions()
+        self.set_options()
 
     @property
     def options(self):
         return self._options
 
-    @classmethod
-    def set_options(cls) -> None:
+    def set_options(self) -> None:
         """Set options to Chrome webdriver."""
 
         list_options = ['disable-infobars', 'start-maximized',
@@ -32,15 +30,30 @@ class AmazonPriceCrawler (AbstractCrawler):
                         'disable-blink-features=AutomationControlled']
 
         for option in list_options:
-            cls._options.add_argument(option)
+            self._options.add_argument(option)
 
-        cls._options.add_experimental_option('excludeSwitches',
-                                             ['enable-automation'])
+        self._options.add_experimental_option('excludeSwitches',
+                                              ['enable-automation'])
 
     def crawl(self):
         """Scrape and return the price of a product."""
 
+    @staticmethod
+    def clean_data(*args) -> float:
+        """Clean data and return float number."""
 
-if __name__ == '__main__':
-    crawler = AmazonPriceCrawler()
-    print(crawler.options.arguments)
+        if len(args) != 2:
+            raise ValueError('Unexpected number of arguments.')
+
+        if any(not isinstance(x, str) for x in args):
+            raise ValueError('Unexpected type of arguments.')
+
+        if not args[0].endswith('.'):
+            raise ValueError('Unexpected first argument for whole part of price.')
+
+        try:
+            price = float(f'{args[0]}{args[1]}')
+        except ValueError as err:
+            raise ValueError('Can not convert value to float.') from err
+
+        return price
