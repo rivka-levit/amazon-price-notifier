9798ede46047978d5635b0a63071f3d13d2d3e2c
rivka-yuliia <juliacreadora@gmail.com>
2023-12-24 23:47:10 +0200

    reformat

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 import time
-
-from flask import Flask, render_template, request, redirect, url_for
 import threading
 
+from flask import Flask, render_template, request, redirect, url_for
+
 from utils.watcher import AmazonPriceWatcher
+
 
 app = Flask(__name__)
 
@@ -14,9 +15,14 @@ def index():
         return render_template('index.html')
 
     if request.method == 'POST':
-        def do_watching(product_url: str):
+
+        def do_watching(product_url: str) -> None:
+            """Create and run the watcher."""
+
             watcher = AmazonPriceWatcher(product_url)
             watcher.watch()
+
+        # Create new thread and run the do_watching() function there.
 
         thread = WatchThread(
             target=do_watching,
@@ -24,6 +30,8 @@ def index():
         )
         thread.start()
         time.sleep(1)
+
+        # Check if an error has been stored in messages.
 
         if thread.messages:
             messages = thread.messages
@@ -36,16 +44,22 @@ def index():
 
 @app.route('/started')
 def started():
+    """Return success page after the watching process has been started."""
+
     return render_template('started.html')
 
 
 @app.route('/error')
 def error():
+    """Return error page if an error occurs."""
+
     messages = request.args.get('messages')
+
     return render_template('error.html', messages=messages)
 
 
 class WatchThread(threading.Thread):
+    """Custom Thread to catch error messages from a separate thread."""
 
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs=None, *, daemon=None):
@@ -54,6 +68,8 @@ class WatchThread(threading.Thread):
         self.messages = None
 
     def run(self):
+        """Override run method to handle error messages."""
+
         try:
             super().run()
         except ValueError as e:
